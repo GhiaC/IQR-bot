5f98025196f98aecf24c546aa09f49050333e7d9
hoomanjb <hoomanjavan74@gmail.com>
2019-03-13 20:42:26 +0330

    feat(IQR): add customer_menu function in Bot/bot.py for make custome menu

--- Bot/bot.py
+++ Bot/bot.py
@@ -2,17 +2,34 @@ from telegram import ReplyKeyboardMarkup
 
 from Constants.button_messages import ButtonMessage
 from Constants.bot_messages import BotMessage
+from Constants.common import BotState
 from Utils.logger import iqr_bot_logger
+from Utils.general_handlers import getting_user_info
 
 
 def start(bot, update):
     general_message = BotMessage.start
     reply_keyboard = [[ButtonMessage.start]]
     update.message.reply_text(general_message, reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    return BotState.start
 
 
 def help(bot, update):
-    update.message.reply_text(BotMessage.quide)
+    general_message = BotMessage.quide
+    reply_keyboard = [[ButtonMessage.start, ButtonMessage.return_to_main_menu]]
+    update.message.reply_text(general_message, reply_markup=ReplyKeyboardMarkup(reply_keyboard))
+    return BotState.help
+
+
+def customer_menu(bot, update):
+    chat_id = getting_user_info(update)
+    reply_keyboard = [[ButtonMessage.show_stores, ButtonMessage.show_discounts]]
+    bot.send_message(chat_id, BotMessage.customer_menu, reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard))
+    return BotState.customer_menu
+
+
+def send_location(bot, update):
+    pass
 
 
 def error(bot, update):
--- Constants/bot_messages.py
+++ Constants/bot_messages.py
@@ -2,3 +2,4 @@ class BotMessage:
     start = "با سلام به بازوی IQR خوش آمدید."
     quide = """به بازوی IQR خوش آمدید."""
     return_to_main_menu = "بازگشت به فهرست اصلی"
+    customer_menu = "لطفا یکی از موارد زیر را انتخاب کنید:"
