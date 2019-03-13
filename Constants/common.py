eee01090470da95ebde2b3d0f62a87743c32efad
hoomanjb <hoomanjavan74@gmail.com>
2019-03-13 20:41:12 +0330

    feat(IQR): add Constants/common.py for using common variables & pattern

---
+++ Constants/common.py
@@ -0,0 +1,21 @@
+class UserData:
+    stores_list = "stores_list"
+
+
+class UserState:
+    start = "start"
+    help = "help"
+    customer_menu = "customer_menu"
+    send_location = "send_location"
+    show_stores = "show_stores"
+
+
+class BotState:
+    start = "START"
+    help = "HELP"
+    menu = "MENU"
+    customer_menu = "CUSTOMER_MENU"
+
+
+class Pattern:
+    absolute = "^{}$"
