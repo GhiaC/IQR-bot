389d95e0d65c20e468a046578a03d00b3987f865
hoomanjb <hoomanjavan74@gmail.com>
2019-03-13 14:03:58 +0330

    feat(IQR): add Constants/bot_messages.py & Constants/button_messages.py and define some variables for first start

---
+++ Constants/bot_messages.py
@@ -0,0 +1,3 @@
+class BotMessage:
+    quide = """به بازوی IQR خوش آمدید."""
+    return_to_main_menu = "بازگشت به فهرست اصلی"
---
+++ Constants/button_messages.py
@@ -0,0 +1,3 @@
+class ButtonMessage:
+    guide = "راهنما"
+    return_to_main_menu = "🔄 بازگشت به فهرست اصلی"
