be19e6b7c75f0bbcec416851e6ca57ee8d652966
hoomanjb <hoomanjavan74@gmail.com>
2019-03-13 20:39:42 +0330

    feat(IQR): add Utils/bot_handlers.py conversation_handler for passing functions to make menu

--- Utils/bot_handlers.py
+++ Utils/bot_handlers.py
@@ -1,8 +1,27 @@
-from telegram.ext import CommandHandler
-from Bot.bot import start, error, help
+from telegram.ext import CommandHandler, ConversationHandler, RegexHandler
+
+from Bot.bot import start, error, customer_menu, send_location
+from Constants.bot_command import Command
+from Constants.common import Pattern, BotState
+from Constants.button_messages import ButtonMessage
 
 
 def bot_handlers(dp):
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help))
     dp.add_error_handler(error)
+    conversation_handler = ConversationHandler(entry_points=[CommandHandler(Command.start, start)], states={
+
+            BotState.start: [RegexHandler(pattern=Pattern.absolute.format(ButtonMessage.start),
+                                          callback=customer_menu)],
+
+            BotState.help: [RegexHandler(pattern=Pattern.absolute.format(ButtonMessage.return_to_main_menu),
+                                         callback=customer_menu, pass_user_data=True)],
+
+            BotState.customer_menu: [RegexHandler(pattern=Pattern.absolute.format(ButtonMessage.show_stores),
+                                                  callback=send_location, pass_user_data=True),
+                                     RegexHandler(pattern=Pattern.absolute.format(ButtonMessage.show_discounts),
+                                                  callback=send_location, pass_user_data=True)]
+        },
+
+        fallbacks=[CommandHandler(Command.cansel, customer_menu)]
+    )
+    dp.add_handler(conversation_handler)
